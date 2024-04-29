50e2f2f5828de1e2545fb7bf983364471edc113d
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-29 11:40:27 +0100

    REFACTOR (functions): adds setReminder basic structure, clean up redundant code from the source repo

--- src/functions/setReminder/index.ts
+++ src/functions/setReminder/index.ts
@@ -1,35 +1,25 @@
 import { formatJSONResponse } from "@libs/apiGw";
 import { APIGatewayProxyEvent } from "aws-lambda";
-import { ulid } from 'ulid';
 import { dynamo } from "@libs/dynamo";
 
 export async function handler(event: APIGatewayProxyEvent) {
     try {
-        const SHORTCODE_LEN = 8;
-        const tableName = process.env?.URL_TABLE;
-        const baseUrl = process.env?.BASE_URL;
+        const tableName = process.env?.REMINDER_TABLE;
 
         if (!tableName) throw Error('Table name value cannot be found!')
 
         const body = JSON.parse(event.body);
 
-        const { url } = body;
-
-        const code = ulid().slice(0, SHORTCODE_LEN);
-
-        const shortUrl = `${baseUrl}/${code}`;
 
         const ddbData = {
-            id: code,
-            shortUrl,
-            url
+
         }
 
         await dynamo.write(ddbData, tableName);
 
         return formatJSONResponse({
             data: {
-                shortUrl, url
+
             },
         })
     } catch (err) {
