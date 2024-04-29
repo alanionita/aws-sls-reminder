91bd5596ddeaf85cc8f80bd932d107fe5cf65363
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-29 14:20:17 +0100

    FEAT (func/sendReminder): adds handler logic
    Add dynamodb util for unmarshalling data

--- src/functions/sendReminder/index.ts
+++ src/functions/sendReminder/index.ts
@@ -1,8 +1,20 @@
 import { DynamoDBStreamEvent } from "aws-lambda";
+import { unmarshall } from "@aws-sdk/util-dynamodb"
+import { AttributeValue } from "@aws-sdk/client-dynamodb";
 
 export async function handler(event: DynamoDBStreamEvent) {
     try {
-      
+        const reminderPromises = event.Records.map(async record => {
+            const data = unmarshall(record.dynamodb.OldImage as Record<string, AttributeValue>)
+            const { email, phoneNo, reminder } = data;
+            if (phoneNo) {
+                await sendSMS(phoneNo, reminder);
+            }
+            if (email) {
+                await sendEmail(email, reminder);
+            }
+        })
+        await Promise.all(reminderPromises)
     } catch (err) {
         console.error(err.message)
         console.info(JSON.stringify(err.stack))
